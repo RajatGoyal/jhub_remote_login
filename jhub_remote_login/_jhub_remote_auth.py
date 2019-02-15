9e2d2538da9652bc6c8dbadff93a52adb2ffb0d7
Juan Cruz-Benito <juan.cruz@ibm.com>
2019-02-15 17:59:56 +0100

    Removing import of RSATools

--- jhub_remote_login/_jhub_remote_auth.py
+++ jhub_remote_login/_jhub_remote_auth.py
@@ -3,7 +3,7 @@ from tornado import gen, web
 from jupyterhub.auth import Authenticator
 from jupyterhub.handlers import BaseHandler
 from jupyterhub.utils import url_path_join
-from encryption import RSATools
+# from encryption import RSATools
 import base64
 
 
