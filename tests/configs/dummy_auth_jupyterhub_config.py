c47ecbeaa06615a2bf00f151017a16e99c4bebad
Juan Cruz-Benito <juan.cruz@ibm.com>
2019-02-21 11:05:01 +0100

    Removing unneeded files

--- tests/configs/dummy_auth_jupyterhub_config.py
+++
@@ -1,9 +0,0 @@
-"""A simple jupyter config file for testing the authenticator."""
-
-c = get_config()
-
-c.JupyterHub.ip = '0.0.0.0'
-c.JupyterHub.hub_ip = '0.0.0.0'
-c.JupyterHub.authenticator_class =\
-    'jhub_remote_login.DummyAuthenticator'
-c.DummyAuthenticator.password = 'password'
